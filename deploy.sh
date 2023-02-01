6f923b7ba18f25414f35f4c5a0dfe042124914a8
Jake Wilkinson <realrobotshk@gmail.com>
2023-02-01 23:15:37 +0800

    added deploy.sh file, need to test in production

---
+++ deploy.sh
@@ -0,0 +1,4 @@
+scp ./public/configurator.html ./joystickshop/public/
+mv ./joystickshop/public/configurator.html ./joystickshop/views/configurator.ejs
+
+scp -r ./public/configuratorFiles/ ./joystickshop/public/
